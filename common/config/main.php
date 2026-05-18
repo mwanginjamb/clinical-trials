51538cfb2dc05972cb2ad008e660ad7bafd272e3
Francis Njambi <fnjambi@outlook.com>
2026-05-18 21:42:40 +0300

    Add mail configs on main config file

--- common/config/main.php
+++ common/config/main.php
@@ -20,5 +20,35 @@ return [
         'wizard' => [
             'class' => \common\library\WizardComponent::class,
         ],
+        'mailer' => [
+            'class' => \yii\symfonymailer\Mailer::class,
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default.
+            'useFileTransport' => false,
+            // You have to set
+            //
+            // 'useFileTransport' => false,
+            //
+            // and configure a transport for the mailer to send real emails.
+            //
+            // SMTP server example:
+                'transport' => [
+                    'scheme' => 'smtp',
+                    'host' => env('SMTP_HOST'),
+                    'username' => env('SMTP_USERNAME'),
+                    'password' => env('SMTP_PASSWORD'),
+                    'port' => 465,
+                   // 'dsn' => 'native://default',
+                ],
+            //
+            // DSN example:
+            //    'transport' => [
+            //        'dsn' => 'smtp://'.env('SMTP_USERNAME').':'.env('SMTP_PASSWORD').'@'.env('SMTP_HOST').':25',
+            //   ],
+            //
+            // See: https://symfony.com/doc/current/mailer.html#using-built-in-transports
+            // Or if you use a 3rd party service, see:
+            // https://symfony.com/doc/current/mailer.html#using-a-3rd-party-transport
+        ],
     ],
 ];
